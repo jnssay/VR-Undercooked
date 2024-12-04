35f4c2db375b534dae5ac8d899614e08f0750637
Polaris106 <wangxingrui2134@gmail.com>
2024-12-04 15:18:13 +0800

    added home scene

--- Assets/Scripts/OpenPauseMenu.cs
+++ Assets/Scripts/OpenPauseMenu.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class MenuControl : MonoBehaviour
+public class OpenPauseMenu : MonoBehaviour
 {
     public GameObject menu;
-    
+
     public InputActionReference open_menu_action;
     public bool menu_is_active = false;
 
@@ -28,15 +28,15 @@ public class MenuControl : MonoBehaviour
         open_menu_action.action.performed -= ToggleMenu;
         InputSystem.onDeviceChange -= OnDeviceChange;
     }
-        
+
     public void ToggleMenu(InputAction.CallbackContext context)
     {
         Debug.Log("menu toggled");
         if (!menu_is_active)
         {
             Debug.Log("menu is active");
+            
             menu_is_active = true;
-            menu.SetActive(true);
             game_control.stage_ongoing = false;
 
             // Set the menu's position in front of the camera
@@ -44,18 +44,17 @@ public class MenuControl : MonoBehaviour
             {
                 Vector3 positionInFrontOfCamera = mainCamera.transform.position + mainCamera.transform.forward * 1.0f;
                 positionInFrontOfCamera += mainCamera.transform.up * -0.2f;
-                menu.transform.position = positionInFrontOfCamera;
-                menu.transform.rotation = Quaternion.LookRotation(mainCamera.transform.forward);
+                Instantiate(menu, positionInFrontOfCamera, Quaternion.LookRotation(mainCamera.transform.forward));
             }
         }
         else
         {
             Debug.Log("menu is inactive");
             menu_is_active = false;
-            menu.SetActive(false);
             game_control.stage_ongoing = true;
+
         }
-        
+
     }
 
     private void OnDeviceChange(InputDevice device, InputDeviceChange change)
@@ -67,11 +66,11 @@ public class MenuControl : MonoBehaviour
                 open_menu_action.action.performed -= ToggleMenu;
                 break;
             case (InputDeviceChange.Reconnected):
-            {
+                {
                     open_menu_action.action.Enable();
                     open_menu_action.action.performed += ToggleMenu;
                     break;
-            }
+                }
         }
     }
 }
