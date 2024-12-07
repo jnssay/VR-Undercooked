aed3fa59f13541e7047b62d0280717948d254d95
kiithy <thekeithchua@gmail.com>
2024-12-07 20:46:44 +0800

    create flow for plating of burger

---
+++ Assets/Scripts/Plating.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Plating : MonoBehaviour
+{
+    public GameObject burgerPrefab;
+    public GameObject soupPrefab;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BurgerPrefinish"))
+        {
+            Debug.Log("Plating burger");
+            PlateBurger(other);
+        }
+        if (other.CompareTag("SoupPrefinish"))
+        {
+            Debug.Log("Plating Soup");
+            PlateSoup(other);
+        }
+    }
+
+    private void PlateBurger(Collider other)
+    {
+        Vector3 position = gameObject.transform.position;
+        Quaternion rotation = gameObject.transform.rotation;
+        GameObject burger = Instantiate(burgerPrefab, position, rotation);
+        Destroy(other.gameObject);
+        // KillPlate();
+        Destroy(gameObject);
+    }
+
+    private void PlateSoup(Collider other)
+    {
+        Vector3 position = other.transform.position;
+        Quaternion rotation = other.transform.rotation;
+        GameObject burger = Instantiate(soupPrefab, position, rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+}
