622ab3e9a3bf6953eb4a63684bb78a1e4055c733
Polaris106 <wangxingrui2134@gmail.com>
2024-12-11 20:55:14 +0800

    changed sizes for some of the game objects (stove, pot)

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    public Transform spawn_point_1;
-    public Transform spawn_point_2;
-
-    private int chosen_spawn_point;
+    public Transform spawn_point;
 
     // Start is called before the first frame update
     void Start()
@@ -25,15 +22,7 @@ public class PlayerRespawn : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            chosen_spawn_point = Random.Range(1, 2);
-            if (chosen_spawn_point == 1)
-            {
-                other.gameObject.transform.position = spawn_point_1.position;
-            }
-            else if (chosen_spawn_point == 2)
-            {
-                other.gameObject.transform.position = spawn_point_2.position;
-            }
+            other.gameObject.transform.position = spawn_point.position;
         }
     }
 }
