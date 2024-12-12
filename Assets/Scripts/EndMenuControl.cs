2763aff419b01dd544d67bb841d9e05a6e7f5a28
Kelly <kellycheongkahyen@gmail.com>
2024-12-12 12:29:47 +0800

    endmenu

--- Assets/Scripts/EndMenuControl.cs
+++ Assets/Scripts/EndMenuControl.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EndMenuControl : MonoBehaviour
 {
     public GameObject end_menu;
 
     private GameControl game_control;
+    public TextMeshProUGUI finalScoreText;
+    public TextMeshProUGUI finalRankText;
+    public TextMeshProUGUI objectivesCompletedText;
+    public TextMeshProUGUI oredersMissedText;
+
+    public float score;
+    public int complete;
+    public int miss;
+    public string rank;
+    public box scoreController;
+
+    private bool isDataFetched = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +33,50 @@ public class EndMenuControl : MonoBehaviour
         if (game_control.stage_clear)
         {
             end_menu.SetActive(true);
-
+            if (isDataFetched)
+            {
+                finalScoreText.text = $"Score: {score}";
+                finalRankText.text = $"Rank: {rank}";
+                objectivesCompletedText.text = $"Orders Completed: {complete}";
+                oredersMissedText.text = $"Orders Missed: {miss}";
+            }
+            else
+            {
+                GetData(scoreController);
+            }
         }
         else
         {
             end_menu.SetActive(false);
         }
+    }
+
+    void GetData(box scoreData)
+    {
+        score = scoreData.PlayerScore;
+        miss = scoreData.missCount;
+        complete = scoreData.completeCount;
+        
+        if(score > 300)
+        {
+            rank = "A";
+        }
+        else if (score > 200)
+        {
+            rank = "B";
+        }
+        else if (score > 100)
+        {
+            rank ="C";
+        }
+        else if (score > 0)
+        {
+            rank ="D";
+        }
+        else
+        {
+            rank = "F";
+        }
+        isDataFetched = true;
     }
 }
